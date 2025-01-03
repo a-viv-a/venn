cdf5b78359ad7c4b02a3c649dd8b0076b750c363
Aviva Ruben <aviva@rubenfamily.com>
2025-01-02 21:54:15 -0800

    working cursor walk

--- src/routes/profile/[handle].tsx
+++ src/routes/profile/[handle].tsx
@@ -1,19 +1,73 @@
 import { Title } from "@solidjs/meta";
 import { createAsync, useParams } from "@solidjs/router";
-import { ParentComponent, Suspense } from "solid-js";
-import { getFollowers, getProfile } from "~/agent";
+import { batch, createEffect, createSignal, ParentComponent, Suspense, untrack } from "solid-js";
+import { getFollowers, getFollows, getProfile } from "~/agent";
 
 const SuspenseArticle: ParentComponent = props => <article><Suspense fallback={<progress />}>{props.children}</Suspense></article>
+
+
+const createCursorReduction = <TRetVal, TAccumulator>(
+  fn: (cursor: string | undefined) => Promise<TRetVal>,
+  extractCursor: (value: TRetVal | undefined) => string | undefined,
+  reducer: (previous: TAccumulator, currentValue: TRetVal) => TAccumulator,
+  initialValue: TAccumulator
+) => {
+  const [accumulator, setAccumulator] = createSignal<TAccumulator>(initialValue)
+  const [cursor, setCursor] = createSignal<string | undefined>()
+  const [isDone, setDone] = createSignal(false)
+  const partial = createAsync(() => fn(cursor()))
+  createEffect(() => {
+    const partialValue = partial()
+    const newCursor = extractCursor(partialValue)
+    console.log({ oldCursor: untrack(cursor), newCursor: newCursor })
+    console.log("effect", partialValue)
+    batch(() => {
+      if (partialValue !== undefined) {
+        setAccumulator(previous => reducer(previous, partialValue))
+      }
+      if (newCursor === undefined) {
+        setDone(true)
+        return
+      }
+      setDone(false)
+      setCursor(newCursor)
+    })
+  })
+
+  return {
+    data: accumulator,
+    isDone
+  }
+}
+
+const getDids = (pviews: { did: string }[]) => pviews.map(pview => pview.did)
+
 
 export default function Handle() {
   const params = useParams<{ handle: string }>()
   const profile = createAsync(() => getProfile({
     actor: params.handle
   }))
-  const followers = createAsync(() => getFollowers({
-    actor: params.handle,
-    limit: 100
-  }))
+  const followers = createCursorReduction(
+    (cursor) => getFollowers({
+      actor: params.handle,
+      limit: 100,
+      cursor
+    }),
+    v => v?.data.cursor,
+    (acc, val) => new Set([...acc, ...getDids(val.data.followers)]),
+    new Set<string>()
+  )
+  const follows = createCursorReduction(
+    (cursor) => getFollows({
+      actor: params.handle,
+      limit: 100,
+      cursor
+    }),
+    v => v?.data.cursor,
+    (acc, val) => new Set([...acc, ...getDids(val.data.follows)]),
+    new Set<string>()
+  )
   return (
     <>
       <Title>{`@${params.handle}`}</Title>
@@ -24,9 +78,8 @@ export default function Handle() {
         } ratio
       </SuspenseArticle>
       <SuspenseArticle>
-        {followers()?.data.followers.map(f => f.did)}
-        <p>{followers()?.data.followers.length}</p>
-        <p>{followers()?.data.cursor}</p>
+        <p>{followers.data().size} followers {followers.isDone() ? "true" : "false"}</p>
+        <p>{follows.data().size} following {follows.isDone() ? "true" : "false"}</p>
       </SuspenseArticle>
     </>
   );
