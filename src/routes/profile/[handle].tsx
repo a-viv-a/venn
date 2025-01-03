6ced44a8a73caa3c2a58ad046079658dd6d0f262
Aviva Ruben <aviva@rubenfamily.com>
2025-01-02 19:35:08 -0800

    fiddle around

--- src/routes/profile/[handle].tsx
+++ src/routes/profile/[handle].tsx
@@ -1,21 +1,33 @@
 import { Title } from "@solidjs/meta";
 import { createAsync, useParams } from "@solidjs/router";
-import { getProfile } from "~/agent";
+import { ParentComponent, Suspense } from "solid-js";
+import { getFollowers, getProfile } from "~/agent";
 
-export default function About() {
+const SuspenseArticle: ParentComponent = props => <article><Suspense fallback={<progress />}>{props.children}</Suspense></article>
+
+export default function Handle() {
   const params = useParams<{ handle: string }>()
   const profile = createAsync(() => getProfile({
     actor: params.handle
   }))
+  const followers = createAsync(() => getFollowers({
+    actor: params.handle,
+    limit: 100
+  }))
   return (
     <>
       <Title>{`@${params.handle}`}</Title>
-      <article>
-        <h2>{profile()?.data.displayName}</h2>
-        <p><small><code>{profile()?.data.followersCount}</code> followers / <code>{profile()?.data.followsCount}</code> following = <code>{
-          ((profile()?.data.followersCount ?? 0) / (profile()?.data.followsCount ?? 0)).toFixed(3)
-        }</code></small></p>
-      </article>
+      <SuspenseArticle>
+        <h2>{`@${params.handle}`}</h2>
+        <sup>{profile()?.data.followersCount} followers</sup>&frasl;<sub>{profile()?.data.followsCount} following</sub> = {
+          ((profile()?.data.followersCount ?? 0) / (profile()?.data.followsCount ?? 1)).toFixed(3)
+        } ratio
+      </SuspenseArticle>
+      <SuspenseArticle>
+        {followers()?.data.followers.map(f => f.did)}
+        <p>{followers()?.data.followers.length}</p>
+        <p>{followers()?.data.cursor}</p>
+      </SuspenseArticle>
     </>
   );
 }
