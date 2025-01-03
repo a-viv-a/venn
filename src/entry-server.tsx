55cbbf4781c896a312dffacb92003a066c4d2120
Aviva Ruben <aviva@rubenfamily.com>
2025-01-02 16:44:12 -0800

    init pt3

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,11 +8,12 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="color-scheme" content="light dark" />
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
         <body>
-          <div id="app">{children}</div>
+          <main class="container-fluid" id="app">{children}</main>
           {scripts}
         </body>
       </html>
