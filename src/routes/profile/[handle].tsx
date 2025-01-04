0abd45c2a532dddacc7c9fcb8de68a92dc0888f3
Aviva Ruben <aviva@rubenfamily.com>
2025-01-03 22:44:11 -0800

    fix 3rd typo. oops

--- src/routes/profile/[handle].tsx
+++ src/routes/profile/[handle].tsx
@@ -131,7 +131,7 @@ export default function Handle() {
                     }
               )
             }} onFinishRender={() => {
-              if (!untrack(rendering)) setRendering(false)
+              if (untrack(rendering)) setRendering(false)
             }} />
           </Show>
         </SuspenseProgress>
