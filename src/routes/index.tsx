0d55eb231b580e0915e2b7b9d3f8eac1388157ee
Aviva Ruben <aviva@rubenfamily.com>
2025-01-04 23:03:50 -0800

    clean up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Title>venn</Title>
       <h1>venn diagram</h1>
       <p>
-        Go forth! Generate your very own venn diagram! Or go <A href="/about">read about this site.</A>
+        Go forth! Generate your very own venn diagram!
       </p>
       <HandleInput />
     </main>
