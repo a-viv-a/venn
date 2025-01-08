2ad1dfd4b18ec7eaed3c999bf99aba0edf0bfeb0
Aviva Ruben <aviva@rubenfamily.com>
2025-01-08 02:20:26 -0800

    give og background color

--- src/getVennSVG.ts
+++ src/getVennSVG.ts
@@ -5,10 +5,14 @@ export const getVennSVG = () => {
   if (documentVenn === null) return undefined
   const venn = documentVenn.cloneNode(true) as SVGAElement
 
+  const style = window.getComputedStyle(venn)
+
   // make the svg legal for standalone rendering
   venn.setAttribute("xmlns", "http://www.w3.org/2000/svg")
   // add some margin to not get cut off
   venn.style.margin = "20px"
+  // make background consistent!
+  venn.style.background = style.getPropertyValue("--pico-card-background-color")
   // these are only for interactivity, we can drop them
   venn.querySelectorAll("g.venn-intersection").forEach(g => venn.removeChild(g))
   // in the document this is inhereted but we need to set it for the rendering
